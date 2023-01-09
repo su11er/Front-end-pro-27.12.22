4f2569b9897fed9428ab43c52ab3f28a226c9850
su11er <wukovnikita89@gmail.com>
2023-01-09 16:38:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var x=10;
-alert('Текст');
-alert(x);
+let name = prompt('Введите ваше имя:');
+let age = prompt('Введите важ возраст:', 18);
+let smoke = confirm("Вы курите?")
+if (age >= 18 && smoke == true) {
+    alert(`Привет, ${name}, что ж, ты сам выбрал такой путь...`)
+}
+else if(age >= 18 && smoke == false) {
+    alert(`Привет ${name}, здорово! Ты наверное еще и спортом занимаешься!`)
+}
+else if (age < 18 && smoke == true) {
+    alert(`Привет ${name}! А родители знают об этом?`)
+}
+else if (age < 18 && smoke == false) {
+    alert(`Привет ${name}! Верно, сейчас главное хорошо учиться!`)
+}
