e6112512765804d928a5a5873b5a9b28ada4ff8b
su11er <wukovnikita89@gmail.com>
2023-01-09 19:28:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var x=10;
-alert('Текст');
-alert(x);
+let x = prompt('Введите первое число:');
+x = parseInt(x)
+let y = prompt('Введите второе число:');
+y = parseInt(y)
+let operator = prompt('Введите оператор (1=+, 2=-, 3=*, 4=/)');
+let result;
+
+if (operator==1) {
+    alert(result = x+y);
+ }
+ else if (operator==2) {
+    alert(result = x-y);
+ }
+ else if (operator==3) {
+    alert(result = x*y);
+ }
+ else if (operator==4) {
+    alert(result = x/y);
+ }
