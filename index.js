951cd077671b5d74f6d4d6f3fd11751225471bb3
su11er <wukovnikita89@gmail.com>
2023-01-30 17:15:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var x=10;
-alert('Текст');
-alert(x);
+function removeElement(arr, value) {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+console.log(removeElement([1,2,3,4,5,6,7,,8,9,0], 5)) // сюда ввести любые значения
