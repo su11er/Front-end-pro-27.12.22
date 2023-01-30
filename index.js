61d2960033ac5d5e5f36a73d10023de5512b2852
su11er <wukovnikita89@gmail.com>
2023-01-30 17:23:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var x=10;
-alert('Текст');
-alert(x);
+function generateKey(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+console.log(generateKey(8)); // в скобках ввести любое число, это будет количеством символов в вашем "пароле"
