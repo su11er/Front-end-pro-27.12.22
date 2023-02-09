de67e71012ca547e7dbecd37a9209568d97a9133
su11er <wukovnikita89@gmail.com>
2023-02-09 12:57:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var x=10;
-alert('Текст');
-alert(x);
+function closures (number1) {
+    console.log(`sum(${number1}) = ${number1}`);
+    return function (number2) {
+        const result = number1 + number2
+        console.log(`sum(${number2}) = ${result}`);
+        return function (number3) {
+            const resultTwo = result + number3;
+            console.log(`sum(${number3}) = ${resultTwo}`)
+        }
+    }
+}
+closures(3)(5)(20);
