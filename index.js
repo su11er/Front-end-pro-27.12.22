d3a755b0ca5636f363d887908f4a4c6cb162ceb4
su11er <wukovnikita89@gmail.com>
2023-02-09 12:53:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var x=10;
-alert('Текст');
-alert(x);
+function pow(x,n) {
+	if (n != 1) {
+	  return x *= pow(x,n - 1);
+	} else {
+	  return x;
+	}
+}
+console.log( pow(3,3)); //сюда ввести любые значения
