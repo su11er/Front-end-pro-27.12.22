de6ea4031c5e54270d8125b6523076ba01786157
su11er <wukovnikita89@gmail.com>
2023-02-28 19:53:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-var x=10;
-alert('Текст');
-alert(x);
+class Student {
+    constructor(firstName, lastName, birthDay, marks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.marks = marks;
+        this.absence = new Array(25);
+        this.absenceIndex = 0;
+        this.age = this.getAge(birthDay);
+        this.averageMark = this.marks.reduce((r, m) => r + m) / this.marks.length;
+        this.presenceFactor = 0.9;
+        this.goodMarksMin = 90;
+        this.results = {
+            BAD: "Редиска!",
+            NORMAL: "Добре, але можна краще.",
+            GOOD: "Молодець!"
+        };
+    }
+
+    absent() {
+        if (this.absence.length > this.absenceIndex) {
+            this.absence[this.absenceIndex] = false;
+            this.absenceIndex++;
+        }
+    };
+
+    present() {
+        if (this.absence.length > this.absenceIndex) {
+            this.absence[this.absenceIndex] = true;
+            this.absenceIndex++;
+        }
+    };
+
+    get averagePresence() {
+        let precenceCount = this.absence.slice(0, this.absenceIndex).filter(x => x).length;
+        return precenceCount / this.absenceIndex;
+    }    
+
+    getAge(dateString) {
+        let today = new Date();
+        let birthDate = new Date(dateString);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    summary() {
+        if (this.averageMark < this.goodMarksMin && this.averagePresence < this.presenceFactor) {
+            console.log(this.results.BAD);
+        } else if (this.averageMark < this.goodMarksMin || this.averagePresence < this.presenceFactor)
+            console.log(this.results.NORMAL);
+        else
+            console.log(this.results.GOOD);
+    }
+}
