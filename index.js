78e5d519e1efa0aeb21f8ce580b4b36b2eea720c
su11er <wukovnikita89@gmail.com>
2023-03-21 19:15:29 +0200

    hw15

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var x=10;
-alert('Текст');
-alert(x);
+﻿const addBtn = document.querySelector('#add-btn');
+const removeBtn = document.querySelector('#remove-btn');
+const container = document.querySelector('#container');
+
+let count = 0;
+
+addBtn.addEventListener('click', () => {
+  count++;
+  const textDiv = document.createElement('div');
+  textDiv.textContent = `Добавленный div ${count}`;
+  textDiv.id = `text-div-${count}`;
+  container.appendChild(textDiv);
+
+  textDiv.addEventListener('click', () => {
+    textDiv.classList.toggle('active');
+  });
+});
+
+removeBtn.addEventListener('click', () => {
+  const lastDiv = container.lastElementChild;
+  if (lastDiv.id.startsWith('text-div-')) {
+    container.removeChild(lastDiv);
+  }
+});
