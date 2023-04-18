63a92175bf939b1d9ad0e7da534b3347cbad9939
su11er <wukovnikita89@gmail.com>
2023-04-18 09:34:19 +0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="main-container">
+        <LeftNavigation />
+        <CentralContainer />
+      </div>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Моделі смартфонів</h1>
+    </header>
+  );
+}
+
+function LeftNavigation() {
+  return (
+    <aside className="left-navigation">
+      <ul>
+        <li><a href="#">Samsung</a></li>
+        <li><a href="#">Apple</a></li>
+        <li><a href="#">Xiaomi</a></li>
+      </ul>
+    </aside>
+  );
+}
+
+function CentralContainer() {
+  return (
+    <main className="central-container">
+      <h2>Список моделей смартфонів</h2>
+      <ul>
+        <li>Samsung Galaxy S21</li>
+        <li>Samsung Galaxy Note 20</li>
+        <li>iPhone 12 Pro</li>
+        <li>iPhone SE</li>
+        <li>Xiaomi Mi 11</li>
+        <li>Xiaomi Redmi Note 9</li>
+      </ul>
+    </main>
   );
 }
 
