d527ee52049b200f5a89c2e531d859e5e031b26c
su11er <wukovnikita89@gmail.com>
2023-04-18 09:17:16 +0300

    .

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com';
+import React from 'react';
 
-const postIdInput = document.getElementById('postId');
-const getPostBtn = document.getElementById('getPostBtn');
-const postContainer = document.getElementById('postContainer');
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main-container">
+        <LeftNavigation />
+        <CentralContainer />
+      </div>
+    </div>
+  );
+}
 
-getPostBtn.addEventListener('click', () => {
-  const postId = postIdInput.value;
-  fetch(`${apiUrl}/posts/${postId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(post => {
-      const postEl = document.createElement('div');
-      postEl.innerHTML = `
-        <h2>${post.title}</h2>
-        <p>${post.body}</p>
-        <button id="getCommentsBtn">Get Comments</button>
-        <div id="commentsContainer"></div>
-      `;
-      postContainer.innerHTML = '';
-      postContainer.appendChild(postEl);
-      const getCommentsBtn = postEl.querySelector('#getCommentsBtn');
-      const commentsContainer = postEl.querySelector('#commentsContainer');
-      getCommentsBtn.addEventListener('click', () => {
-        fetch(`${apiUrl}/posts/${postId}/comments`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(comments => {
-            const commentsEl = document.createElement('div');
-            comments.forEach(comment => {
-              commentsEl.innerHTML += `
-                <h4>${comment.name}</h4>
-                <p>${comment.body}</p>
-              `;
-            });
-            commentsContainer.innerHTML = '';
-            commentsContainer.appendChild(commentsEl);
-          })
-          .catch(error => {
-            console.error('Error fetching comments:', error);
-            commentsContainer.innerHTML = '<p>Error fetching comments</p>';
-          });
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching post:', error);
-      postContainer.innerHTML = '<p>Error fetching post</p>';
-    });
-});
+function Header() {
+  return (
+    <header>
+      <h1>Моделі смартфонів</h1>
+    </header>
+  );
+}
+
+function LeftNavigation() {
+  return (
+    <aside>
+      <ul>
+        <li><a href="#">Samsung</a></li>
+        <li><a href="#">Apple</a></li>
+        <li><a href="#">Xiaomi</a></li>
+      </ul>
+    </aside>
+  );
+}
+
+function CentralContainer() {
+  return (
+    <main>
+      <h2>Список моделей смартфонів</h2>
+      <ul>
+        <li>Samsung Galaxy S21</li>
+        <li>Samsung Galaxy Note 20</li>
+        <li>iPhone 12 Pro</li>
+        <li>iPhone SE</li>
+        <li>Xiaomi Mi 11</li>
+        <li>Xiaomi Redmi Note 9</li>
+      </ul>
+    </main>
+  );
+}
+
+export default App;
