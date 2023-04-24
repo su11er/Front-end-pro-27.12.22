026cbfea9a5c2d914b4d847765ed572e029e74a8
su11er <wukovnikita89@gmail.com>
2023-04-24 10:58:24 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,52 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const initialEmojis = [
-  { id: 1, image: '😀', count: 0 },
-  { id: 2, image: '😍', count: 0 },
-  { id: 3, image: '🤔', count: 0 },
-  { id: 4, image: '😂', count: 0 },
-  { id: 5, image: '🥰', count: 0 },
-];
+function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Зробити домашнє завдання', done: false },
+    { text: 'Приготувати обід', done: true },
+    { text: 'Погуляти з собакою', done: false },
+  ]);
 
-const EmojiList = () => {
-  const [selectedEmoji, setSelectedEmoji] = useState(null);
-  const [showResults, setShowResults] = useState(false);
-  const [emojis, setEmojis] = useState(initialEmojis);
+  const [newTodo, setNewTodo] = useState('');
 
-  const handleVote = (id) => {
-    const updatedEmojis = emojis.map((emoji) =>
-      emoji.id === id ? { ...emoji, count: emoji.count + 1 } : emoji
-    );
-    setSelectedEmoji(id);
-    setEmojis(updatedEmojis);
-  };
+  function handleTodoClick(index) {
+    const newTodos = [...todos];
+    newTodos[index].done = !newTodos[index].done;
+    setTodos(newTodos);
+  }
 
-  const handleShowResults = () => {
-    const winningEmoji = emojis.reduce((prev, current) =>
-      prev.count > current.count ? prev : current
-    );
-    setShowResults(true);
-    setSelectedEmoji(winningEmoji.id);
-  };
+  function handleNewTodoChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleAddTodoClick() {
+    const newTodos = [...todos, { text: newTodo, done: false }];
+    setTodos(newTodos);
+    setNewTodo('');
+  }
 
   return (
     <div>
-      <h2>Emoji List</h2>
-      {emojis.map((emoji) => (
-        <div key={emoji.id} onClick={() => handleVote(emoji.id)}>
-          <span style={{ fontSize: '2rem' }}>{emoji.image}</span>
-          <span style={{ marginLeft: '1rem' }}>{emoji.count}</span>
-          {selectedEmoji === emoji.id && <span>✔️</span>}
-        </div>
-      ))}
-      <button onClick={handleShowResults}>Show Results</button>
-      {showResults && (
-        <div>
-          <h3>Winning Emoji:</h3>
-          <span style={{ fontSize: '2rem' }}>
-            {emojis.find((emoji) => emoji.id === selectedEmoji)?.image}
-          </span>
-        </div>
-      )}
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+            onClick={() => handleTodoClick(index)}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+      <form>
+        <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+        <button type="button" onClick={handleAddTodoClick}>
+          Add
+        </button>
+      </form>
     </div>
   );
-};
+}
 
-export default EmojiList;
+export default App;
